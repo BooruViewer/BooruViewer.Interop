2bcc9915683619418e2de6d70f99201b0b3c0a1d
Nikey646 <nikey646@hotmail.com>
2020-10-14 23:27:42 +1100

    Actually initialize properties with values provided in the constructor of the SourceBooru class

--- Dtos/Booru/SourceBooru.cs
+++ Dtos/Booru/SourceBooru.cs
@@ -4,13 +4,15 @@ namespace BooruViewer.Interop.Dtos.Booru
 {
     public class SourceBooru
     {
-        public Uri BaseUri { get; set; }
-        public String Name { get; set; }
-        public String Identifier { get; set; }
+        public Uri BaseUri { get; }
+        public String Name { get; }
+        public String Identifier { get; }
 
         public SourceBooru(String id, String name, Uri baseUri)
         {
-
+            this.Identifier = id;
+            this.Name = name;
+            this.BaseUri = baseUri;
         }
     }
 }

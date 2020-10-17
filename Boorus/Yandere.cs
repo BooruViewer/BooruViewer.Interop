0ad272472aa98fb415b4082bbf3d46dd6ac999d8
Nikey646 <nikey646@hotmail.com>
2020-10-17 23:34:52 +1100

    Implemented the Moebooru tag cache using Microsoft.Extensions.Caching.Abstractions instead of Dictionay<String, TagTypes> Fixed errors with the tag cache not being used and requests to the tag.json being run multiple times when not necessary.

--- Boorus/Yandere.cs
+++ Boorus/Yandere.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using BooruViewer.Interop.Boorus.Abstract;
 using BooruViewer.Interop.Dtos.Booru;
 using BooruViewer.Interop.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace BooruViewer.Interop.Boorus
 {
@@ -11,7 +12,7 @@ namespace BooruViewer.Interop.Boorus
         private static SourceBooru _sourceBooru = new SourceBooru("yandere", "Yande.re", new Uri("https://yande.re/"));
 
         // ReSharper disable once SuggestBaseTypeForParameter
-        public Yandere(IYandereApi api) : base(api, _sourceBooru)
+        public Yandere(IYandereApi api, IDistributedCache cache) : base(api, _sourceBooru, cache)
         { }
     }
 }
